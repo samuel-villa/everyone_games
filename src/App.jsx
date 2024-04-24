64e0c6106bc7551c1d8b0f385bdeaff6b18d987c
samuel-villa <ciullasamuel@gmail.com>
2024-04-24 16:59:46 +0200

    [GIT] first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+const API_KEY = import.meta.env.VITE_API_KEY
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [games, setGames] = useState([])
+
+  let searchApi = `https://api.rawg.io/api/games?key=${API_KEY}`
+
+  const getGames = fetch(searchApi)
+  .then(response => response.json())
+  .then(data => setGames(data.results))
+  .catch(error => console.error(error))
+
+  getGames()
+    
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul>
+        <li>{games}</li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
