6d68cbc9e48b3ecabf6dbb7b563719d56e143bd3
samuel-villa <ciullasamuel@gmail.com>
2024-04-25 09:37:22 +0200

    [ADD] code structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './scss/style.scss'
 const API_KEY = import.meta.env.VITE_API_KEY
 
 
