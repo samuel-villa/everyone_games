c46984567c09573eeff9cedf69d3dd0231126545
samuel-villa <ciullasamuel@gmail.com>
2024-04-25 11:28:54 +0200

    [ADD] game card

---
+++ src/components/GameCard.jsx
@@ -0,0 +1,12 @@
+export function GameCard({ game }) {
+  return (
+    <div className="card">
+      <div className="card2">
+        { game.name }
+        { game.name }
+        { game.name }
+        { game.name }
+      </div>
+    </div>
+  );
+}
--- src/components/GamesList.jsx
+++ src/components/GamesList.jsx
@@ -1,10 +1,13 @@
+import { GameCard } from "./GameCard.jsx";
+
 export function GamesList({ games }) {
   return (
-    <ul>
+    <ul className="games-list">
       {games.map((game) => (
-        <li key={game.id}>
-          <label htmlFor={game.id}>{game.name}</label>
-        </li>
+        // <li key={game.id}>
+        //   <label htmlFor={game.id}>{game.name}</label>
+          <GameCard game={ game }/>
+        // </li>
       ))}
     </ul>
   );
