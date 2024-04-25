e99853185509a1d3fcfbcab5b257f6ca48eb688f
samuel-villa <ciullasamuel@gmail.com>
2024-04-25 12:27:09 +0200

    [IMP] game card info from API

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -2,10 +2,22 @@ export function GameCard({ game }) {
   return (
     <div className="card">
       <div className="card2">
-        { game.name }
-        { game.name }
-        { game.name }
-        { game.name }
+        <img src={game.background_image} alt="game-bg-img"></img>
+        <h2>{game.name}</h2>
+        <ul className="platforms">
+            {game.parent_platforms.map((pf) => (
+                <li key={pf.platform.id}>{pf.platform.name}</li>
+            ))}
+        </ul>
+        <p>{game.added}</p>
+        <p>{game.metacritic}</p>
+        <p>{game.rating}</p>
+        <p>{game.released}</p>
+        <ul className="genres">
+            {game.genres.map((genre) => (
+                <li key={genre.id}>{genre.name}</li>
+            ))}
+        </ul>
       </div>
     </div>
   );
--- src/components/GamesList.jsx
+++ src/components/GamesList.jsx
@@ -4,10 +4,7 @@ export function GamesList({ games }) {
   return (
     <ul className="games-list">
       {games.map((game) => (
-        // <li key={game.id}>
-        //   <label htmlFor={game.id}>{game.name}</label>
-          <GameCard game={ game }/>
-        // </li>
+        <GameCard key={game.id} game={game} />
       ))}
     </ul>
   );
