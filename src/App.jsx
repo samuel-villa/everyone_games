9dcc6535aece83b400cbd6f216b9bb20482068c0
samuel-villa <ciullasamuel@gmail.com>
2024-04-26 15:22:50 +0200

    [IMP] games by genre

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import axios from 'axios';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
@@ -6,6 +7,7 @@ import "./scss/style.scss";
 import HomePage from "./pages/HomePage";
 import Genres from "./pages/Genres";
 import Game from "./pages/Game";
+import GameByGenre from "./pages/GamesByGenre";
 
 
 const API_KEY = import.meta.env.VITE_API_KEY;
@@ -16,47 +18,48 @@ function App() {
   const [games, setGames] = useState([])
   const [genres, setGenres] = useState([])
   const [platforms, setPlatforms] = useState([])
-  
-    useEffect(() => {
-      const apiCall = `https://api.rawg.io/api/games?key=${API_KEY}`;
-      axios.get(apiCall)
-        .then(response => {
-          setGames(response.data.results);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }, []);
 
-    useEffect(() => {
-        const apiCall = `https://api.rawg.io/api/genres?key=${API_KEY}`;
-        axios.get(apiCall)
-          .then(response => {
-            setGenres(response.data.results);
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      }, []);
+  useEffect(() => {
+    const apiCall = `https://api.rawg.io/api/games?key=${API_KEY}`;
+    axios.get(apiCall)
+      .then(response => {
+        setGames(response.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
-      useEffect(() => {
-        const apiCall = `https://api.rawg.io/api/platforms?key=${API_KEY}`;
-        axios.get(apiCall)
-          .then(response => {
-            setPlatforms(response.data.results);
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      }, []);
+  useEffect(() => {
+    const apiCall = `https://api.rawg.io/api/genres?key=${API_KEY}`;
+    axios.get(apiCall)
+      .then(response => {
+        setGenres(response.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const apiCall = `https://api.rawg.io/api/platforms?key=${API_KEY}`;
+    axios.get(apiCall)
+      .then(response => {
+        setPlatforms(response.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage games={ games } genres={ genres } platforms={ platforms }/>}/>
-        <Route path="/genres" element={<Genres genres={ genres }/>}/>
-        <Route path="/game/:id" element={<Game />}/>
+        <Route path="/" element={<HomePage games={games} genres={genres} platforms={platforms} />} />
+        <Route path="/genres" element={<Genres genres={genres} />} />
+        <Route path="/game/:id" element={<Game />} />
+        <Route path="/genre/:id" element={<GameByGenre games={games} setGames={setGames} genres={genres} platforms={platforms}/>} />
       </Routes>
     </Router>
   );
--- src/components/GenreCard.jsx
+++ src/components/GenreCard.jsx
@@ -1,14 +1,19 @@
+import { Link } from "react-router-dom";
+
+
 export function GenreCard({ genre }) {
   
   return (
-    <div className="genreCard">
-      <p className="heading">Popular this month</p>
-      <div>
-        <p>Games</p>
-        <p>{genre.games_count}</p>
-        <p>{genre.id}</p>
+    <Link to={`/genre/${genre.id}`}>
+      <div className="genreCard">
+        <p className="heading">Popular this month</p>
+        <div>
+          <p>Games</p>
+          <p>{genre.games_count}</p>
+          <p>{genre.id}</p>
+        </div>
+        <p>{genre.name}</p>
       </div>
-      <p>{genre.name}</p>
-    </div>
+    </Link>
   );
 }
