d47b54cbc826e90719daa360af2b88b6f132e4d0
samuel-villa <ciullasamuel@gmail.com>
2024-04-26 13:50:22 +0200

    [IMP] pointer

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 export function SideBar() {
     return (
       <section className="sidebar">
-        <ul>
-          <li><Link to="/genres">Genres</Link></li>
-          <li>New releases</li>
-          <li>Next week releases</li>
-          <li>Popular Games</li>
-        </ul>
+          <ul>
+            <li><Link to="/genres">Genres</Link></li>
+            <li>New releases</li>
+            <li>Next week releases</li>
+            <li>Popular Games</li>
+          </ul>
        
       </section>
     );
