1df919360a3a6867039587551f1b2d377e5e9120
samuel-villa <ciullasamuel@gmail.com>
2024-04-26 11:39:33 +0200

    [IMP] header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export function Header() {
     return (
-      <header>
+      <header className="container">
         <h1>EveryoneGames</h1>
         <Link to="/games">Go to details</Link>
       </header>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
 export function SideBar() {
     return (
       <section className="sidebar">
-        <h1>SideBAr</h1>
         <ul>
           <li><Link to="/genres">Genres</Link></li>
         </ul>
