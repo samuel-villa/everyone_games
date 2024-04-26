2822a9863be1d9d746e8a5d79059a56f49ba60df
samuel-villa <ciullasamuel@gmail.com>
2024-04-26 15:26:42 +0200

    [IMP] games by genre

--- src/components/GamesList.jsx
+++ src/components/GamesList.jsx
@@ -2,11 +2,6 @@ import { GameCard } from "./GameCard.jsx";
 
 export function GamesList({ games, genres, platforms }) {
 
-  const getFilteredGamesByGenre = (genre) => {
-    
-  }
-  const getFilteredGamesByPlatform = (platform) => {}
-
   return (
     <ul className="games-list container">
       {games.map((game) => (
