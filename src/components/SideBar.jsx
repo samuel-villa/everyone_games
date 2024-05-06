1c508f2343f27ca748da06a8a9dcdd63ce2131d9
samuel-villa <ciullasamuel@gmail.com>
2024-05-06 15:24:41 +0200

    [ADD] icons

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,18 +1,20 @@
 import React from "react";
+import { MdCategory, MdStarRate } from "react-icons/md";
+import { GiPlatform } from "react-icons/gi";
 import { Link } from "react-router-dom";
 
 
 export function SideBar() {
     return (
-      <section className="sidebar">
+      <section className="sidebar container">
           <ul>
-            <li><Link to="/genres">Genres</Link></li>
+            <li><Link to="/genres"><MdCategory />Genres</Link></li>
             <li>New releases</li>
             <li>Next week releases</li>
-            <li><Link to="/popular-games">Popular Games</Link></li>
+            <li><Link to="/popular-games"><MdStarRate />Popular Games</Link></li>
           </ul>
           <ul>
-            <li><Link to="/platforms">Platforms</Link></li>
+            <li><Link to="/platforms"><GiPlatform />Platforms</Link></li>
           </ul>
        
       </section>
