c61aac66d2b899bb53685dd250a2e8004abeed08
samuel-villa <ciullasamuel@gmail.com>
2024-05-06 16:53:57 +0200

    [IMP] sections titles

--- src/components/GenresList.jsx
+++ src/components/GenresList.jsx
@@ -2,10 +2,13 @@ import { GenreCard } from "./GenreCard.jsx";
 
 export function GenresList({ genres }) {
   return (
-    <ul className="genres-list container">
-      {genres.map((genre) => (
-        <GenreCard key={genre.id} genre={genre} />
-      ))}
-    </ul>
+    <div>
+      <h2>Genres</h2>
+      <ul className="genres-list container">
+        {genres.map((genre) => (
+          <GenreCard key={genre.id} genre={genre} />
+        ))}
+      </ul>
+    </div>
   );
 }
--- src/components/PlatformsList.jsx
+++ src/components/PlatformsList.jsx
@@ -2,10 +2,13 @@ import { PlatformCard } from "./PlatformCard.jsx";
 
 export function PlatformsList({ platforms }) {
   return (
-    <ul className="genres-list container">
-      {platforms.map((platform) => (
-        <PlatformCard key={platform.id} platform={platform} />
-      ))}
-    </ul>
+    <div>
+      <h2>Platforms</h2>
+      <ul className="genres-list container">
+        {platforms.map((platform) => (
+          <PlatformCard key={platform.id} platform={platform} />
+        ))}
+      </ul>
+    </div>
   );
 }
