b5ec53839ed6926798fd781545eb6e74fcbe4196
samuel-villa <ciullasamuel@gmail.com>
2024-05-07 10:37:52 +0200

    [IMP] game card display hidden info

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -26,43 +26,47 @@ export function GameCard({ game }) {
     const getPlatformLogo = (slug) => {
         const platformLogo = platformsLogos[slug];
         return platformLogo || null;
-      };
+    };
 
-  return (
-    <Link to={`/game/${game.id}`} >
-    <article className="gameCard">
-      <div className="gameCard2">
-        <div className="gameCard__shown">
-            <div className="imgCont"><img src={game.background_image} alt="game-bg-img"></img></div>
-            <div className="visibleDesc">
-                <ul className="platforms">
-                    {game.parent_platforms.map((pf) => (
-                        pf.platform.slug in platformsLogos && (
-                            <li key={pf.platform.id}>
-                                {getPlatformLogo(pf.platform.slug)}
-                            </li>
-                        )
-                    ))}
-                </ul>
-                <h2>{game.name}</h2>
-                <button className="added">
-                    <FaPlus />
-                    <p>{game.added}</p>
-                </button>
-            </div>
-        </div>
-        <div className="gameCard__hidden">
-            <p>{game.metacritic}</p>
-            <p>{game.rating}</p>
-            <p>{game.released}</p>
-            <ul className="genres">
-                {game.genres.map((genre) => (
-                    <li key={genre.id}>{genre.name}</li>
-                ))}
-            </ul>
-        </div>
-      </div>
-    </article>
-    </Link>
-  );
+    return (
+        <Link to={`/game/${game.id}`} >
+            <article className="gameCard">
+                <div className="gameCard2">
+                    <div className="gameCard__shown">
+                        <div className="imgCont"><img src={game.background_image} alt="game-bg-img"></img></div>
+                        <div className="visibleDesc">
+                            <ul className="platforms">
+                                {game.parent_platforms.map((pf) => (
+                                    pf.platform.slug in platformsLogos && (
+                                        <li key={pf.platform.id}>
+                                            {getPlatformLogo(pf.platform.slug)}
+                                        </li>
+                                    )
+                                ))}
+                            </ul>
+                            <h2>{game.name}</h2>
+                            <button className="added">
+                                <FaPlus />
+                                <p>{game.added}</p>
+                            </button>
+
+                            <div className="gameCard__hidden">
+                                <ul>
+                                    <li><p>Rate:</p><p>{game.rating}</p></li>
+                                    <li><p>Release Date:</p><p>{game.released}</p></li>
+                                    <ul className="genres">
+                                        <p>Genres: </p>
+                                        {game.genres.map((genre) => (
+                                            <li key={genre.id}>{genre.name}</li>
+                                        ))}
+                                    </ul>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </article>
+        </Link>
+    );
 }
