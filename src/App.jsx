4808d7c07819a3f9e651202777cae300060a93e8
samuel-villa <ciullasamuel@gmail.com>
2024-05-08 10:25:56 +0200

    [IMP] cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import axios from 'axios';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
